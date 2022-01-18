51c113507b5c1be5d8b4f21ab66b81f790c9b595
moletsane2 <katleho.mokoroane@bothouniversity.com>
2022-01-18 18:18:04 +0200

    toggles message

---
+++ JS/state.js
@@ -0,0 +1,8 @@
+function togglebt() {
+  var x = document.getElementById("st");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
